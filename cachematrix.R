ea8d542c6cf82783d2a31be008a689f48edc3566
André Tannús <andre.tannus@gmail.com>
2015-06-17 23:13:36 -0300

    Implements Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,72 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following pair of functions work together.
+##
+## The first of the pair, makeCacheMatrix, constructs
+## a wrapper object that can hold a matrix and cache
+## its inverse.
+## The second function is used to get the cached inverse
+## of the matrix in the object created with the first
+## function. If an inverse value in not yet cached, it is
+## computed, then cached.
 
-## Write a short comment describing this function
-
+# Function 1:
+# Creates a wrapper object capable of caching
+# the inverted matrix. The object also exposes
+# a getters and a setters for the matrix and inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+    # Initialize variable to hold the inverted matrix.
+    xinv = NULL;
+    
+    # Matrix setter.
+    set <- function(matrix) {
+        x <<- matrix;
+        xinv <<- NULL;
+    }
+    
+    # Matrix getter.
+    get <- function() {
+        # Variable x is defined in the environment
+        # in which this function is declared and is
+        # therefore captured in the closure.
+        x;
+    }
+    
+    # Inverse setter.
+    setinv <- function(inversematrix) {
+        xinv <<- inversematrix;
+    }
+    
+    # Inverse getter.
+    getinv <- function() {
+        xinv;
+    }
+    
+    # Returns wrapper object.
+    list(set = set, get = get, setinv = setinv, getinv = getinv);
 }
 
 
-## Write a short comment describing this function
-
+# Takes a wrapper object created by makeCacheMatrix()
+# as an argument and returns the inverse of the matrix 
+# within that object. If a cached inverse is not found,
+# one is computed and stored.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    # Get the cached inverse matrix.
+    xinv <- x$getinv();
+    
+    # Check if the value in non-null.
+    if( ! is.null(xinv) ) {
+        # If so, return it (with a message!).
+        message("getting cached data");
+        return(xinv);
+    }
+    
+    # If the value was null, get the matrix.
+    matrix <- x$get();
+    
+    # Invert it.
+    inversematrix <- solve(matrix, ...);
+    
+    # Cache the inverse value and return it.
+    x$setinv(inversematrix);
+    inversematrix;
 }
